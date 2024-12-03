3ccc4b46c848fba2002908856de2f2877dcab753
prandika119 <an.prasetya9@gmail.com>
2024-12-03 13:30:14 +0700

    Data pada dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaksi;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        $transaksi_count =
-        return view('dashboard', );
+        $transaksi_count = Transaksi::count();
+        return view('dashboard', compact('transaksi_count'));
     }
 }
