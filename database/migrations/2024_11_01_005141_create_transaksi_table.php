ce210bb91abf7b2723f092cbd57d57a60b96e2ec
prandika119 <an.prasetya9@gmail.com>
2024-12-03 12:50:26 +0700

    Migration tabel transaksi menggunakan softdelete dan timestamp

--- database/migrations/2024_11_01_005141_create_transaksi_table.php
+++ database/migrations/2024_11_01_005141_create_transaksi_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -11,12 +12,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema:create('transaksi', function (Blueprint $table) {
+        Schema::create('transaksi', function (Blueprint $table) {
             $table->id();
             $table->date('tanggal_pembelian');
             $table->integer('total_harga');
             $table->integer('bayar');
             $table->integer('kembalian');
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
