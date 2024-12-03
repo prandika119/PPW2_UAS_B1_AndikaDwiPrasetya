2d119ec87a887b8865e8f91dd632a2087c70c8e1
prandika119 <an.prasetya9@gmail.com>
2024-12-03 14:00:38 +0700

    View login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,3 @@
-
 <div class="row justify-content-center mt-5">
     <div class="col-md-8">
 
@@ -13,20 +12,23 @@
                 <form action="{{ route('authenticate') }}" method="post">
                     @csrf
                     <div class="mb-3 row">
-                        <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email Address</label>
+                        <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email
+                            Address</label>
                         <div class="col-md-6">
-                            <input type="email" class="form-control @error('') is-invalid @enderror" name="" value="{{ old('') }}">
-                            @if ($errors->has(''))
-                                <span class="text-danger">{{ $errors->first('') }}</span>
+                            <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                name="email" value="{{ old('email') }}">
+                            @if ($errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
                             @endif
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="password" class="col-md-4 col-form-label text-md-end text-start">Password</label>
                         <div class="col-md-6">
-                            <input type="password" class="form-control @error('') is-invalid @enderror" name="">
-                            @if ($errors->has(''))
-                                <span class="text-danger">{{ $errors->first('') }}</span>
+                            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                name="password">
+                            @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
                             @endif
                         </div>
                     </div>
