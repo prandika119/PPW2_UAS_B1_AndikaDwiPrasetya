d8adfde9b8fa4afe9af2212d23cb638c0f751391
prandika119 <an.prasetya9@gmail.com>
2024-12-03 13:33:08 +0700

    Controller Transaksi function edit

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -67,7 +67,7 @@ class TransaksiController extends Controller
     public function edit($id)
     {
         $transaksi = Transaksi::findOrFail($id);
-        return view('transaksi.edit',);
+        return view('transaksi.edit', compact('transaksi'));
     }
 
     public function update(Request $request)
