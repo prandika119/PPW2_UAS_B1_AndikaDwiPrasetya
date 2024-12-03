d0fa1b3f0d0cf109587bf036296525dc59c97f19
prandika119 <an.prasetya9@gmail.com>
2024-12-03 14:09:37 +0700

    View dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,6 +9,8 @@ class DashboardController extends Controller
     public function index()
     {
         $transaksi_count = Transaksi::count();
-        return view('dashboard', compact('transaksi_count'));
+        $omzet = Transaksi::sum('total_harga');
+        $jml_item = Transaksi::sum('bayar');
+        return view('dashboard', compact('transaksi_count', 'omzet', 'jml_item'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,3 @@
-
 <div class="d-flex flex-column gap-4">
     <div class="card">
         <div class="card-header">SUMMARY</div>
@@ -8,7 +7,9 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Jumlah Transaksi</h5>
-                            <h4><span class="badge text-bg-secondary">{{ number_format($transaksi_count, 0, '.', '.') }}</span></h4>
+                            <h4><span
+                                    class="badge text-bg-secondary">{{ number_format($transaksi_count, 0, '.', '.') }}</span>
+                            </h4>
                         </div>
                     </div>
                 </div>
@@ -16,7 +17,7 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Jumlah Item Terjual</h5>
-                            <h4><span class="badge text-bg-secondary"></span></h4>
+                            <h4><span class="badge text-bg-secondary">{{ $jml_item }}</span></h4>
                         </div>
                     </div>
                 </div>
@@ -24,7 +25,7 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Omzet</h5>
-                            <h4><span class="badge text-bg-secondary"></span></h4>
+                            <h4><span class="badge text-bg-secondary">{{ $omzet }}</span></h4>
                         </div>
                     </div>
                 </div>
@@ -36,9 +37,12 @@
         <div class="card-header">DATA</div>
         <div class="card-body">
             <div class="list-group">
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Tambah Transaksi</a>
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Lihat Transaksi</a>
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Lihat Transaksi Detail</a>
+                <a href="{{ route('transaksi.create') }}" class="list-group-item list-group-item-action">Tambah
+                    Transaksi</a>
+                <a href="{{ route('transaksi.index') }}" class="list-group-item list-group-item-action">Lihat
+                    Transaksi</a>
+                <a href="{{ route('transaksidetail.index') }}" class="list-group-item list-group-item-action">Lihat
+                    Transaksi Detail</a>
             </div>
         </div>
     </div>
